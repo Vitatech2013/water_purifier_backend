e9b92ec362fffb112d401861bf4d427f2450ccb9
Srikanth Yaswada <109497462+srikanthyaswada@users.noreply.github.com>
2024-09-03 15:56:01 +0530

    port changed

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use("/api/sale", salesRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
