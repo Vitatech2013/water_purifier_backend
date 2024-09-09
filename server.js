226958a9ecd31ddc2433e7d8d4fe84e0b0e2caba
Srikanth Yaswada <109497462+srikanthyaswada@users.noreply.github.com>
2024-09-09 14:59:37 +0530

    port changed to 7000

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use("/api/product", productRoutes);
 app.use("/api/sale", saleRoutes);
 app.use("/api/service", serviceTypeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
