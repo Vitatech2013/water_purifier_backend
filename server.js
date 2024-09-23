e13105a8526a18a9f845533fd6675614b801c456
Srikanth Yaswada <109497462+srikanthyaswada@users.noreply.github.com>
2024-09-23 15:34:24 +0530

    updated

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const productRoutes = require("./routes/productRoutes");
 const saleRoutes = require("./routes/saleRoutes");
 const serviceTypeRoutes = require("./routes/serviceRoutes");
 const userRoutes = require("./routes/userRoutes");
-const technicianRoutes = require('./routes/technicianRoutes');
-const authRoutes = require("./routes/authRoutes")
+const technicianRoutes = require("./routes/technicianRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 connectDB();
@@ -29,14 +29,12 @@ app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/owner", ownerRoutes);
-app.use('/api/technician', technicianRoutes);
+app.use("/api/technician", technicianRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/sale", saleRoutes);
 app.use("/api/service", serviceTypeRoutes);
 app.use("/api/user", userRoutes);
-
-
 
 const PORT = process.env.PORT || 7002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
